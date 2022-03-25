309ff9e79a5e28802609914df294b61d72014596
Gürkan Aydın <g.aydin@digitastic.app>
2022-03-25 14:00:05 +0000

    Add new file

---
+++ dockerPrometheus/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ADDR=$(ip addr | grep -i -e "inet[[:space:]]" | awk 'NR==2{print $2}' | cut -d/ -f1)
+BASE_FILE="./prometheus.base.yml"
+echo $ADDR
+sed "s/__HOST_IP__/$ADDR/g" $BASE_FILE > ./prometheus.yml
