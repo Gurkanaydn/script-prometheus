acd42e447db0e28a8ccd1d133f216fb037866049
Gürkan Aydın <g.aydin@digitastic.app>
2022-03-25 12:58:37 +0000

    Update setup.sh

--- prometheus/setup.sh
+++ prometheus/setup.sh
@@ -2,4 +2,4 @@
 ADDR=$(ip addr | grep -i -e "inet[[:space:]]" | awk 'NR==2{print $2}' | cut -d/ -f1)
 BASE_FILE="./prometheus.base.yml"
 echo $ADDR
-sed "s/__HOST_IP__/$ADDR/g" $BASE_FILE > ./prometheus.new.yml
+sed "s/__HOST_IP__/$ADDR/g" $BASE_FILE > ./prometheus.yml
