191ce64586f630b747e95462b9c36eb5ade287dd
Gürkan Aydın <g.aydin@digitastic.app>
2022-03-29 08:04:50 +0000

    Update install.sh

--- grafanaPrometheus/install.sh
+++ grafanaPrometheus/install.sh
@@ -5,8 +5,8 @@ sudo apt-get install -y software-properties-common wget
 wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 echo "deb https://packages.grafana.com/enterprise/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 echo "deb https://packages.grafana.com/enterprise/deb beta main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
-sudo apt-get update
-sudo apt-get install grafana-enterprise
+sudo apt-get update -y 
+sudo apt-get install grafana-enterprise -y
 sudo systemctl daemon-reload
 sudo systemctl start grafana-server
 sudo systemctl enable grafana-server.service
