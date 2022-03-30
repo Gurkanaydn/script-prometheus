fe68c4bd6a642a6535716f642ef9e346903095f6
Gürkan Aydın <g.aydin@digitastic.app>
2022-03-30 10:47:10 +0000

    Update install.sh

--- dockerPrometheus/install.sh
+++ dockerPrometheus/install.sh
@@ -28,6 +28,6 @@ sudo useradd -rs /bin/false node_exporter
 sudo cp node_exporter.service /lib/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl start node_exporter
-sudo systemctl enable node_exporte
+sudo systemctl enable node_exporter
 
 sudo docker run --volume=/:/rootfs:ro  --volume=/var/run:/var/run:ro  --volume=/sys:/sys:ro  --volume=/var/lib/docker/:/var/lib/docker:ro  --volume=/dev/disk/:/dev/disk:ro  --publish=8080:8080 --detach=true  --name=cadvisor google/cadvisor:latest
