4e4c3318f483e94905a9869ad051aa0acc4d70dd
Gurkan aydın <aydingurkan804@gmail.com>
2022-04-21 06:01:57 +0300

    added crontab configuration

--- proccesScript/install.sh
+++ proccesScript/install.sh
@@ -3,6 +3,7 @@ wget https://github.com/prometheus/pushgateway/releases/download/v1.4.2/pushgate
 tar xvf pushgateway-1.4.2.linux-amd64.tar.gz
 cd pushgateway-1.4.2.linux-amd64
 mv pushgateway /usr/local/bin
+cd ..
 cp pushgateway.service /etc/systemd/system
 systemctl start pushgateway.service
 systemctl enable pushgateway.service
